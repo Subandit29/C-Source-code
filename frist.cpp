70d6119de0c902d206708e0f1527ba5b744f58bf
Subandit29 <jame.keattipan@gmail.com>
2025-07-01 11:57:32 +0700

    Create frist.cpp

---
+++ frist.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  // s using to give white space before the mini heartl
+  string s = "               ";
+  cout  << endl << endl << "\n\n\n\n";
+  
+  /* to show mini heart
+      using both of space and * character */
+  cout <<   s  <<  "     **     ** "       << endl ;
+  cout <<   s  <<  "     *********  "  << endl ;
+  cout <<   s  <<  "      *******  "    << endl ;
+  cout <<   s  <<  "        *** "          << endl ;
+  cout <<   s  <<  "         *  "           << endl ;
+  
+  return 0; 
+}
